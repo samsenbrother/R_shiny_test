8ea37e845a690742dec61110f9db52ea307496b4
samsenbrother <css.qrs@gmail.com>
2014-12-06 10:36:33 +0800

    add the different hearders to the "ui"

--- Build a user-interface/ui.R
+++ Build a user-interface/ui.R
@@ -1,7 +1,22 @@
 shinyUI(fluidPage(
-  titlePanel('Test title'),
+  titlePanel(h1("Build my app")),
   sidebarLayout(position = 'right',
-    sidebarPanel('sidebarPanel'),
-    mainPanel('MainPanel')
+    sidebarPanel(
+      h1("First level title"),
+      h2("Second level title"),
+      h3("Third level title"),
+      h4("Fourth level title"),
+      h5("Fifth level title"),
+      h6("Sixth level title")
+                 ),
+    mainPanel(
+      h6("Episode IV", align = "center"),
+      h6("A NEW HOPE", align = "center"),
+      h5("It is a period of civil war.", align = "center"),
+      h4("Rebel spaceships, striking", align = "center"),
+      h3("from a hidden base, have won", align = "center"),
+      h2("their first victory against the", align = "center"),
+      h1("evil Galactic Empire.")
     )
+  )
 ))
