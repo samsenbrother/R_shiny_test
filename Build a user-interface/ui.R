acbc7e00b861519bfef36aced1c86954d22da02f
samsenbrother <css.qrs@gmail.com>
2014-12-06 16:13:11 +0800

    THE standard code of the homework_Build a UI

--- Build a user-interface/ui.R
+++ Build a user-interface/ui.R
@@ -1,25 +1,35 @@
-#ui.R
+# ui.R
+
 shinyUI(fluidPage(
-  titlePanel(h1("My Shiny App")),
-  sidebarLayout(position = "right",
-      sidebarPanel(
-        h1('Introducing Shiny'),
-        p("shiny is a new package from Rstudio that makes it incredible"),
-        br(),
-        p("For an Introduction and live examples,vist the",strong("Shiny homepage",style="color:blue")),
-        br(),
-        br(),
-        h2("Fetures"),
-        p("*Build useful web applications with only a few lines of code-no JavaScript required"),
-        p("Shiny applications are automatically that ",strong("spreadsheets")," are live,Outputs change instantly as users modify inputs")
-      ),
-      mainPanel(
-        h2("Installation"),
-        p("Shiny is availuble on CRAN,so you can ",br(),"install it in usual way from your R",br(),"console:"),
-        code("install.packages('shiny')"),
-        br(),
-        br(),
-        img(src='bigorb.png',height=150,width=150,"Shiny is a product of Rstudio")
-        )
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(
+      h2("Installation"),
+      p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
+      code('install.packages("shiny")'),
+      br(),
+      br(),
+      br(),
+      br(),
+      img(src = "bigorb.png", height = 72, width = 72),
+      "shiny is a product of ", 
+      span("RStudio", style = "color:blue")
+    ),
+    mainPanel(
+      h1("Introducing Shiny"),
+      p("Shiny is a new package from RStudio that makes it ", 
+        em("incredibly easy"), 
+        " to build interactive web applications with R."),
+      br(),
+      p("For an introduction and live examples, visit the ",
+        a("Shiny homepage.", 
+          href = "http://www.rstudio.com/shiny")),
+      br(),
+      h2("Features"),
+      p("* Build useful web applications with only a few lines of code-no JavaScript required."),
+      p("* Shiny applications are automatically 'live' in the same way that ", 
+        strong("spreadsheets"),
+        " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.")
     )
+  )
 ))
