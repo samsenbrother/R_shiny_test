88c556fce109f473bd909e131994f25d0e4e517b
samsenbrother <css.qrs@gmail.com>
2014-12-06 12:37:39 +0800

    with zhizhi

--- Build a user-interface/ui.R
+++ Build a user-interface/ui.R
@@ -3,7 +3,7 @@
 shinyUI(fluidPage(
   titlePanel("My Shiny App"),
   sidebarLayout(
-    sidebarPanel('zhizhi is sb',p('zhizhi is sb',span('zhizhi is sb ',style="color:blue"),br(),strong("zhizhi is sb"),em('zhizhi is sb',style = "color:blue"))),
+    sidebarPanel('zhizhi is sb',p('zhizhi is sb',span('zhizhi is sb ',style="color:blue"),br(),strong("zhizhi is sb"),em('zhizhi is sb',style = "color:blue"),code('zhizhi is sb')),code("zhizhi is sb",style="color:blue")),
     mainPanel(
       p("p creates a paragraph of text. Note: this paragraph is followed by br(), which makes a blank line."),
       p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph", style = "font-family: 'times'; font-si16pt"),
