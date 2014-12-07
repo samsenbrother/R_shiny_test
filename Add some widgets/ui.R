04a090db4ac25db38cadd5836abea5a48912226e
samsenbrother <css.qrs@gmail.com>
2014-12-07 18:53:51 +0800

    standard answer of homework3

--- Add some widgets/ui.R
+++ Add some widgets/ui.R
@@ -1,20 +1,24 @@
 # ui.R
 
 shinyUI(fluidPage(
-  titlePanel("consusVis"),
+  titlePanel("censusVis"),
+  
   sidebarLayout(
     sidebarPanel(
-      p("Create demographic maps with information from the 2010 US Census."),
-      br(),
-      column(5,
-             selectInput("choose panel",label=strong("Choose a variable to display"),
-              choices=list("Percent White"=1,"Percent Black"=2,"Percent Hispanic"=3,"Percent Asian"=4),selected=1)
-            ),
-      column(5,
-             sliderInput("slider",label=strong("Range of interest"),
-                min=0,max=100,value=c(0,100))      
-            )
-      ),
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+        label = "Choose a variable to display",
+        choices = list("Percent White", "Percent Black",
+          "Percent Hispanic", "Percent Asian"),
+        selected = "Percent White"),
+      
+      sliderInput("range", 
+        label = "Range of interest:",
+        min = 0, max = 100, value = c(0, 100))
+    ),
+    
     mainPanel("")
-    )
+  )
 ))
