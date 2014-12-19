6d765435ebfe5976c9a0555153c2c96f9ffae624
samsenbrother <css.qrs@gmail.com>
2014-12-19 10:28:55 +0800

    version 0.1

--- birtth/server.R
+++ birtth/server.R
@@ -12,16 +12,37 @@ shinyServer(function(input, output) {
   output$bobo <- renderImage({
         if(input$submit !=0){
           if(input$name1 == 'bbb' & input$name2 == 'bobo'){
-          filename<- normalizePath(file.path('www/bobo.png'))
-          list(src=filename)
+            filename<- normalizePath(file.path('www/bobo.JPG'))
+            list(src=filename)
           }
           else
-            filename<- normalizePath(file.path('www/boom.png'))
+            filename<- normalizePath(file.path('www/boom.JPG'))
             list(src=filename)
         }
         else
-          filename<- normalizePath(file.path('www/wenhao.png'))
+          filename<- normalizePath(file.path('www/wenhao.JPG'))
           list(src=filename)
+    
+  },deleteFile=FALSE)
+  output$birth1 <- renderText({
+        if(input$submit !=0){
+          if(input$name1 == 'bbb' & input$name2 == 'bobo')
+            print('Happy birthday BoBo!!')
+        }
+  })
+  output$birth2 <- renderImage({
+    if(input$submit !=0){
+      if(input$name1 == 'bbb' & input$name2 == 'bobo'){
+        filename<- normalizePath(file.path('www/birth.JPG'))
+        list(src=filename)
+      }
+      else
+        filename<- normalizePath(file.path('www/b.JPG'))
+      list(src=filename)
+    }
+    else
+      filename<- normalizePath(file.path('www/a.JPG'))
+    list(src=filename)
     
   },deleteFile=FALSE)
   
--- birtth/ui.R
+++ birtth/ui.R
@@ -5,16 +5,15 @@ shinyUI(fluidPage(
       textInput('name1',label = p('Please Input your name'),value = '你的名字'),
       br(),
       textInput('name2',label = p('Please Input your nickname belongs to ElderSamSen')),
-      img(src="samsen.png",height=150,width=150),
       br(),
+      img(src='samsen.JPG',height=200,width=200),
       actionButton('submit',label=strong('测测'))
       
     ),
     mainPanel(
       h1(textOutput('namecheck'),align='center'),
       imageOutput('bobo',height=300,width=300),
-      br(),
-      textOutput('birth1'),
+      h1(textOutput('birth1')),
       br(),
       imageOutput('birth2',height=300,width=300),
       br()
