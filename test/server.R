9332ea24c0470c499eeedfc652d329338e08e2e6
samsenbrother <css.qrs@gmail.com>
2015-05-18 11:12:52 +0800

    xiugai_2015_5_15

--- test/server.R
+++ test/server.R
@@ -1,26 +1,26 @@
 library(dplyr)
 library(ggplot2)
 source('normalize.R')
+source('plot.R')
+source('score.R')
 shinyServer(function(input, output, session) {
   data<-reactive({
-    input$seller
+    infile <-input$seller
+    if(is.null(infile))
+      return(NULL)
+    tmp<-read.csv(infile$datapath,stringsAsFactor=F)
   })
   
   output$data <- renderTable({
-    infile <- data()
-  if(is.null(infile))
-    return(NULL)
-    
-  a<-read.csv(infile$datapath)
-  a
-    }, options=list(pageLength=10))
+    if(is.null(data()))
+      return(NULL)
+    data()
+  }, options=list(pageLength=10))
   
   output$seller_ui <- renderUI({
-    infile <- data()
-    if(is.null(infile))
+    if(is.null(data()))
       return(NULL)
-    
-    tmp<-read.csv(infile$datapath)
+    tmp<-data()
     tmp %>% select(name) ->name
     name<-as.matrix(name)
     name<-as.vector(name)
@@ -28,31 +28,132 @@ shinyServer(function(input, output, session) {
   })
   
   output$weidu <- renderUI({
-    infile <- data()
-    if(is.null(infile))
+    if(is.null(data()))
       return(NULL)
-    tmp<-read.csv(infile$datapath)
+    tmp<-data()
     name<-names(tmp)
     name<-name[-c(1,2)]
     selectInput("Dim","Dim select",name)
   })
-  #根据选择的卖家和维度作图
-  output$plot <- renderPlot({
-    infile <- data()
-    if(is.null(infile))
+  
+  output$weidu_1 <- renderUI({
+    if(is.null(data()))
       return(NULL)
-    tmp<-read.csv(infile$datapath)
+    tmp<-data()
+    name<-names(tmp)
+    name<-name[-c(1,2)]
+    selectInput("Dim_1","Dim select",name)
+  })
+  #根据选择的卖家和维度作图
+  
+  output$plot <- renderPlot({
+    if(is.null(data()))
+      return(NULL)
+    tmp<-data()
     seller <- input$sellers
-    dim <- input$Dim
+    dim <- input$Dim_1
     #提取需要的数据到数据框先
     number<-names(tmp) %in% dim
     plotdata<-tmp[,number]
     plotdata<-scale(plotdata)
-    as.data.frame(plotdata)->plotdata
-    q<-ggplot(plotdata,aes(x = eval(dim)))
-    q+geom_density()+theme_light()
+    plot(plotdata,dnorm(plotdata,mean(plotdata),sd(plotdata)),xlab = dim,ylab = "density")
   })
   
+  output$mean <- renderText({
+    if(is.null(data()))
+      return(NULL)
+    a<-"Mean:"
+    tmp<-data()
+    dim <- input$Dim
+    number<-names(tmp) %in% dim
+    plotdata<-tmp[,number]
+    mean<-round(mean(plotdata),2)
+    paste(a,mean)
+  })
+  
+  output$level <- renderText({
+    if(is.null(data()))
+      return(NULL)
+    a<-"排名:"
+    b<-"击败了"
+    c<-"%的卖家"
+    tmp<-data()
+    seller <- input$sellers
+    tmp_1<-tmp%>%filter(name==seller)
+    id<-tmp_1["id"]
+    id<-as.vector(as.matrix(id))
+    dim <- input$Dim
+    number<-names(tmp) %in% dim
+    plotdata<-tmp[,number]
+    rank<-rank(plotdata)
+    rank<-length(plotdata)+1-rank
+    rank<-round(rank[id],2)
+    rate<-round((1-rank/length(plotdata))*100,2)
+    paste(a,rank,b,rate,c)
+  })
+  
+  output$seller_jing1 <- renderUI({
+    if(is.null(data()))
+      return(NULL)
+    tmp<-data()
+    tmp %>% select(name) ->name
+    name<-as.matrix(name)
+    name<-as.vector(name)
+    selectInput("s_1","选择一个卖家",name)
+  })
+  
+  output$seller_jing2 <- renderUI({
+    if(is.null(data()))
+      return(NULL)
+    tmp<-data()
+    tmp %>% select(name) ->name
+    name<-as.matrix(name)
+    name<-as.vector(name)
+    selectInput("s_2","选择另一个卖家",name)
+  })
+  
+    output$plot_jing <- renderPlot({
+      if(is.null(data()))
+        return(NULL)
+      tmp<-data()
+      tmp<-tmp[-1]
+      scale<-as.data.frame(scale(tmp[-1]))
+      max=max(scale)
+      min=min(scale)
+      tmp<-cbind(tmp[1],scale)
+      s1<-input$s_1
+      s2<-input$s_2
+      s<-c(s1,s2)
+      tmp %>% filter(name==s[1] | name==s[2]) -> tmp
+      names(tmp)[1] <- "group"
+      CreateRadialPlot(tmp,grid.min = min,grid.max = max)
+    })
 
+  output$table <- renderTable({
+    if(is.null(data()))
+      return(NULL)
+    tmp<-data()
+    tmp1<-tmp[-c(1,2)]
+    sc<-score(tmp1)
+    d<-cbind(tmp[2],sc)
+    rank<-rank(d$sc)
+    cbind(d,rank)->d
+    d%>% mutate(rate=round((1-rank/nrow(tmp))*100,2))->d
+    dd<-d$rate
+    s1<-input$s_1
+    s2<-input$s_2
+    s<-c(s1,s2)
+    d$rate<-paste("击败",dd,"%",sep = '')
+    e<-d %>% filter(name==s[1] | name==s[2])
+    names(e)<-c("店铺名","得分","排名","成就")
+    e
+  })
+  
+  output$fenbu <- renderPlot({
+    if(is.null(data()))
+      return(NULL)
+    ggplot(d,aes(sc))+geom_bar(aes(color=..x..))+xlab("评分")+guides(color="none")+theme_classic()
+  })
+  
   
 })
--- test/ui.R
+++ test/ui.R
@@ -1,25 +1,52 @@
 library(markdown)
 
-shinyUI(navbarPage("卖家竞争力情况",
-                    tabPanel("读取数据",
-                             sidebarLayout(
-                               sidebarPanel(
+shinyUI(navbarPage(theme = "bootstrap.css",
+                   "卖家竞争力情况",
+                    
+                   tabPanel('参考文档',
+                     includeMarkdown('about.md')
+                     ),
+                   tabPanel("读取数据",
+                            sidebarLayout(
+                              sidebarPanel(
                                 fileInput("seller","卖家数据文件.csv",accept = ".csv"
-                                 )
-                               ),
-                               mainPanel(
-                                 uiOutput('seller_ui'), #卖家选择
-                                 uiOutput('weidu'), #维度选择
-                                 plotOutput('summary')
+                                )
+                              ),
+                              mainPanel(
+                                uiOutput('seller_ui'), #卖家选择
+                                uiOutput('weidu'), #维度选择
+                                textOutput('mean'),#平均值
+                                textOutput('level'),#排名
+                                plotOutput('summary')
+                                
+                                
                               )
-                             )
-                    ),
+                            )
+                   ),
                    tabPanel("具体数据",
                             tableOutput('data')
                      ),
                    tabPanel('卖家维度对比',
+                            uiOutput('weidu_1'), #维度选择
                             plotOutput('plot')      
                             
+                     ),
+                   tabPanel('卖家竞争力对比',
+                            sidebarPanel(uiOutput('seller_jing1'), #卖家选择
+                                uiOutput('seller_jing2')
+                                
+                                
+                              
+                            ),
+                            mainPanel(
+                              tableOutput('table'),
+                              plotOutput('plot_jing')
+                              
+                              )
+                     ),
+                   tabPanel("竞争力总分分布情况",
+                            plotOutput("fenbu")
+                     
                      )
                    
                      
